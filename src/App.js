f3aae75de6841b6632a00c458bf9919f1a884cc4
alimaqsood19 <ali-maqsood@hotmail.com>
2021-04-26 18:45:51 -0400

    Added collection item, preview and shoppage components with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import Homepage from './pages/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shoppage.component';
 
 function App() {
   return (
     <div className='App'>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
