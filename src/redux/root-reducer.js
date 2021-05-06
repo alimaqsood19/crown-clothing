87efdc08cd828a7214a7537d5b422732bbc3b460
alimaqsood19 <ali-maqsood@hotmail.com>
2021-05-06 15:11:34 -0400

    Added configuration for Redux Persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,18 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import sessionStorage from 'redux-persist/lib/' --> find session storage if you want that instead
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'], //Only reducer we want to persist is the cart at the moment
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = [logger]; //Array of middlewares (add more or less)
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middlewares))
 ); //Spread operator adds middlewares as individual arguments
-
-export default store;
+export const persistor = persistStore(store); //Creates a persisted version of our store
