0758405c3dd06454476a983c9364a7311aa19939
alimaqsood19 <ali-maqsood@hotmail.com>
2021-05-06 15:22:11 -0400

    Directory reducer and selector created

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage';
 // import sessionStorage from 'redux-persist/lib/' --> find session storage if you want that instead
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -14,5 +15,6 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 export default persistReducer(persistConfig, rootReducer);
